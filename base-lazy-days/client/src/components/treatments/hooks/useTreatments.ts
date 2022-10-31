e55f18ee95b114ce3e03212d4bc82870a7e5f787
Ken <rg4093@naver.com>
2022-10-31 16:38:54 +0900

    section4 완료

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,32 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  // useCustomToast을 사용하여 Toast message로 에러 핸들링
+  // const toast = useCustomToast();
+  const fallback = [];
+  // const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+  //   onError: (error) => {
+  //     const title =
+  //       error instanceof Error
+  //         ? error.message
+  //         : 'error connect to the server...';
+
+  //     toast({ title, status: 'error' });
+  //   },
+  // });
+
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  // queryClient에서 Error handling
+  return data;
 }
