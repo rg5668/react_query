9dc0baa54f02a87ef9b2369a5a63967809d6e1b4
Ken <rg4093@naver.com>
2022-11-12 20:27:36 +0900

    section5

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -1,10 +1,13 @@
 import { Icon, Stack, Text } from '@chakra-ui/react';
+import { usePrefetchTreatments } from 'components/treatments/hooks/useTreatments';
 import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
 import { BackgroundImage } from '../common/BackgroundImage';
 
 export function Home(): ReactElement {
+  // 홈에서 미리 usePrefetchTreatments(프리페치 된 데이터)를 가져와 빠른 동작을 하게 해준다.
+  usePrefetchTreatments();
   return (
     <Stack align="center" justify="center" height="84vh">
       <BackgroundImage />
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -29,4 +29,9 @@ export function useTreatments(): Treatment[] {
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   // queryClient에서 Error handling
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
